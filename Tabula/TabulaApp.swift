29a10cac312fa3173b306288e2f2badf2f23fbcc
Mason <me@masonx.ca>
2022-10-01 18:57:04 -0400

    Initial Commit

---
+++ Tabula/TabulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabulaApp.swift
+//  Tabula
+//
+//  Created by Mason on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TabulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
